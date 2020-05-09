85f67840277e942296f34772c7cf297aebe44db5
Kelsey Maass <kmaass@uw.edu>
2020-05-09 14:59:48 -0700

    adding doses script

--- inspect_doses.py
+++ inspect_doses.py
@@ -1,4 +1,4 @@
-"""Solve for various OAR weights"""
+"""Solve for different doses"""
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -17,13 +17,13 @@ PTV_min = ConstituentFunctions[9]
 Lung_dvh = ConstituentFunctions[11]
 
 # Solve for different lung weights
-print('Solving for different weights')
-weight_vec = 10.0**np.arange(0, 5)
-lung_avg = np.zeros_like(weight_vec)
-ptv_d95 = np.zeros_like(weight_vec)
-for k in range(len(weight_vec)):
-    print('Lung weight:  %e' % weight_vec[k], end=', ')
-    Lung_dvh.DoseFunctionParameters.Weight = weight_vec[k]
+print('Solving for different doses')
+dose_vec = np.arange(0, 3000.0, 500.0)
+lung_avg = np.zeros_like(dose_vec)
+ptv_d95 = np.zeros_like(dose_vec)
+for k in range(len(dose_vec)):
+    print('Lung dose:  %e' % dose_vec[k], end=', ')
+    Lung_dvh.DoseFunctionParameters.DoseLevel = dose_vec[k]
     Patient.Cases[0].TreatmentPlans[1].PlanOptimizations[0].ResetOptimization()
     Patient.Cases[0].TreatmentPlans[1].PlanOptimizations[0].RunOptimization()
     lung_avg[k] = Patient.Cases[0].TreatmentPlans[1].PlanOptimizations[0].\
@@ -36,10 +36,10 @@ for k in range(len(weight_vec)):
 
 # Plot results
 fig, ax = plt.subplots(1, 3, figsize=(15, 5))
-ax[0].plot(weight_vec, lung_avg, 'o')
+ax[0].plot(dose_vec, lung_avg, 'o')
 ax[0].set_xlabel('Lung Weight')
 ax[0].set_ylabel('Lung Average')
-ax[1].plot(weight_vec, ptv_d95, 'o')
+ax[1].plot(dose_vec, ptv_d95, 'o')
 ax[1].set_xlabel('Lung Weight')
 ax[1].set_ylabel('PTV D95')
 ax[2].plot(lung_avg, ptv_d95, 'o')
