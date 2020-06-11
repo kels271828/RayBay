6cb01f6a23f7e45b0e33653ecfee608c08e05d58
Kelsey Maass <kmaass@uw.edu>
2020-06-10 19:23:36 -0700

    save additional values

--- src/sample.py
+++ src/sample.py
@@ -149,8 +149,8 @@ def get_roi_stats(dose, roi):
         Dose statistics for given region of interest.
 
     """
-    volumes = [0.99, 0.98, 0.95, 0.5, 0.05, 0.02, 0.01]
-    volume_names = ['D99', 'D98', 'D95', 'D50', 'D5', 'D2', 'D1']
+    volumes = [0.99, 0.98, 0.95, 0.9, 0.5, 0.1, 0.05, 0.02, 0.01]
+    volume_names = ['D99', 'D98', 'D95', 'D90', 'D50', 'D10', 'D5', 'D2', 'D1']
     stats = {'Min': dose.GetDoseStatistic(RoiName=roi, DoseType='Min'),
              'Max': dose.GetDoseStatistic(RoiName=roi, DoseType='Max'),
              'Average': dose.GetDoseStatistic(RoiName=roi, DoseType='Average')}
@@ -204,31 +204,79 @@ def get_goals(plan, roi_list):
     return results
 
 
+def get_objs(plan):
+    """Get values related to objective terms.
+
+    Totally hard-coded for now.
+
+    Parameters
+    ----------
+    dose : connect.connect_cpython.PyScriptObject
+        Total dose for current treatment plan.
+
+    Returns
+    -------
+    dict
+        Values related to objective terms.
+
+    """
+    dose = plan.TreatmentCourse.TotalDose
+    values = {}
+    values['PTV'] = [{
+        'FunctionType': 'MinDose',
+        'DoseValue': 4800,
+        'PercentVolume': 0,
+        'ResultValue': dose.GetDoseStatistic(RoiName='PTV', DoseType='Min')
+    }, {
+        'FunctionType': 'MaxDose',
+        'DoseValue': 6198,
+        'PercentVolume': 0,
+        'ResultValue': dose.GetDoseStatistic(RoiName='PTV', DoseType='Max')
+    }]
+    values['Rib'] = [{
+        'FunctionType': 'MaxDvh',
+        'DoseValue': 3200, 
+        'PercentVolume': 0.27,
+        'ResultValue': dose.GetDoseAtRelativeVolumes(RoiName='Rib',
+            RelativeVolumes=[0.0027])[0]
+    }]
+    values['SpinalCanal'] = [{
+        'FunctionType': 'MaxDvh',
+        'DoseValue': 2080,
+        'PercentVolume': 0.67,
+        'ResultValue': dose.GetDoseAtRelativeVolumes(RoiName='SpinalCanal',
+            RelativeVolumes=[0.0067])[0]
+    }]
+    values['Heart'] = [{
+        'FunctionType': 'MaxDvh',
+        'DoseValue': 2800,
+        'PercentVolume': 1.84,
+        'ResultValue': dose.GetDoseAtRelativeVolumes(RoiName='Heart',
+            RelativeVolumes=[0.0184])[0]
+    }]
+    values['Chestwall_L'] = [{
+        'FunctionType': 'MaxDvh',
+        'DoseValue': 3000,
+        'PercentVolume': 2.04,
+        'ResultValue': dose.GetDoseAtRelativeVolumes(RoiName='Chestwall_L',
+            RelativeVolumes=[0.0204])[0]
+    }]
+    values['Lungs'] = [{
+        'FunctionType': 'MaxDvh',
+        'DoseValue': 2000,
+        'PercentVolume': 10,
+        'ResultValue': dose.GetDoseAtRelativeVolumes(RoiName='Lungs',
+            RelativeVolumes=[0.1])[0]
+    }]
+    return values
+
+
 if __name__ == '__main__':
     """
-    Objective function terms:
-     *  0, SpinalCanal, MaxDose, 2080
-        1, Esophagus, MaxDose
-     *  2, Heart, MaxDose, 2800
-        3, GreatVes, MaxDose
-        4, Trachea, MaxDose
-        5, Bronchus, MaxDose
-     *  6, Rib, MaxDose, 3200 
-        7, Skin, MaxDose
-     *  8, PTV, MaxDose, 6240
-     *  9, PTV, MinDose, 4800
-     * 10, Chestwall_L, MaxDvh, 3000, 1.5
-     * 11, Lungs, MaxDvh, 2000, 10
-       12, D2cm, MaxDose
-       13, Pericardium, MaxDose
-       14, R1, MaxDose
-       15, R2, MaxDose
-       16, D2cm_CD, MaxDose
-       17, D2cm_CD2, MaxDose
-
     Objective terms = { Name : [{ idx, DoseLevel }] }
     Dose stats = { Name : { blah, blah, blah } }
     Clinical goals = { Name : [{ idx, AcceptanceLevel, GoalCriteria, GoalValue }] }
+    Obj values = { Name : [{ FunctionType, DoseValue, PercentVolume, ResultValue }]}
     """    
     # Get RayStation objects
     plan = connect.get_current('Plan')
@@ -239,31 +287,34 @@ if __name__ == '__main__':
     
     # Set objective function parameters
     par_dict = {
-        'SpinalCanal': [{'idx': 0, 'DoseLevel': 2080, 'Range': [0, 2080]}],
-        'Heart': [{'idx': 2, 'DoseLevel': 2800, 'Range': [0, 2800]}],
-        'Rib': [{'idx': 6, 'DoseLevel': 3200, 'Range': [0, 3200]}],
-        #'PTV': [{'idx': 8, 'DoseLevel': 6240, 'Range': [4801, 6240]}],
-        'Chestwall_L': [{'idx': 10, 'DoseLevel': 3000, 'Range': [0, 3000]}],
-        'Lungs': [{'idx': 11, 'DoseLevel': 2000, 'Range': [0, 2000]}]
+        'PTV': [{'idx': 1, 'DoseLevel': 6200, 'Range': [5500, 6200]}],
+        'Rib': [{'idx': 2, 'DoseLevel': 3200, 'Range': [1600, 3200]}],
+        'SpinalCanal': [{'idx': 3, 'DoseLevel': 2080, 'Range': [1040, 2080]}],
+        'Heart': [{'idx': 4, 'DoseLevel': 2800, 'Range': [1400, 2800]}],
+        'Chestwall_L': [{'idx': 5, 'DoseLevel': 3000, 'Range': [1500, 3000]}],
+        'Lungs': [{'idx': 6, 'DoseLevel': 2000, 'Range': [1000, 2000]}]
     }
 
     # Sample treatment plans
+    n_success = 0
     results = []
     prev_pars = set()
     prev_pars.add(get_pars(par_dict))
     fpath = '\\\\client\\C$\\Users\\Kelsey\\Dropbox (uwamath)\\autoray\\results\\'
-    for ii in range(100):
+    for ii in range(500):
         print(f'Iteration: {ii}')
         if ii > 0:
             sample_pars(par_dict, prev_pars)
         print(get_pars(par_dict))
         set_pars(plan, par_dict)
         flag = calc_plan(plan, beam_set)
-        print(f'Success: {flag}\n')
-        if flag == 0:
-            results.append([copy.deepcopy(par_dict), get_stats(plan, roi_list),
-                            get_goals(plan, roi_list)])
+        n_success = n_success + 1 if flag < 2 else n_success
+        print(f'Success: {flag}, n_success: {n_success}\n')
+        if flag < 2:
+            results.append([flag, copy.deepcopy(par_dict), get_stats(plan, roi_list),
+                            get_goals(plan, roi_list), get_objs(plan)])
         else:
-            results.append([copy.deepcopy(par_dict), flag])
-        np.save(fpath + 'samples_6_8.npy', results)
- 
+            results.append([flag, copy.deepcopy(par_dict)])
+        np.save(fpath + 'samples_6_10.npy', results)
+        if n_success >= 100:
+            break
