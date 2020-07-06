97e76de85f21b493e6f054f61f2a668a1b817214
Kelsey Maass <kmaass@uw.edu>
2020-07-06 16:08:16 -0700

    adding rib grid search for patient 1

--- src/grid_patient.py
+++ src/grid_patient.py
@@ -1,11 +1,12 @@
-"""Compute Grid search of treatment plans for patient 2."""
+"""Compute grid search of treatment plans for patient."""
 import sys
 
 import sample
 
 repo_path = '\\\\client\\C$\\Users\\Kelsey\\Dropbox (uwamath)\\autoray\\'
 sys.path.append(repo_path + 'src\\')
-funcs_path = repo_path + 'results\\patient2\\funcs_grid.csv'
-goals_path = repo_path + 'results\\patient2\\goals.csv'
-save_path = repo_path + 'results\\patient2\\'
-sample.grid_search(funcs_path, 'PTV 4/7/20', 6270, 99, goals_path, save_path)
+funcs_path = repo_path + 'results\\patient1\\funcs_grid.csv'
+goals_path = repo_path + 'results\\patient1\\goals.csv'
+save_path = repo_path + 'results\\patient1\\'
+sample.grid_search(funcs_path, 'PTV', 4800, 95, goals_path, save_path,
+                   n_points=25)
