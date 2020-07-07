8d6462f81273771513ddc5ec81bb617af94f9807
Kelsey Maass <kmaass@uw.edu>
2020-07-07 12:26:46 -0700

    get results from toy prob opts

---
+++ src/p1_toy.py
@@ -0,0 +1,67 @@
+"""Get goal values and dvh from optimal solutions."""
+import sys
+
+import numpy as np
+
+import connect
+import sample
+
+repo_path = '\\\\client\\C$\\Users\\Kelsey\\Dropbox (uwamath)\\autoray\\'
+sys.path.append(repo_path + 'src\\')
+
+
+def get_dvh(plan, roi_names):
+    """Get dvh curves from treatment plan.
+
+    Parameters
+    ----------
+    plan : connect.connect_cpython.PyScriptObjects
+        Current treatment plan.
+    roi_names : list
+        Regions of interest to include in results.
+
+    Returns
+    -------
+    dict
+        Dose and volumes for given regions of interest.
+
+    """
+    dose = plan.TreatmentCourse.TotalDose
+    max_dose = max([dose.GetDoseStatistic(RoiName=roi, DoseType='Max')
+                    for roi in roi_names])
+    dvh = {'Dose': np.linspace(0, max_dose, 100)}
+    for roi in roi_names:
+        dvh[roi] = dose.GetRelativeVolumeAtDoseValues(RoiName=roi,
+                                                      DoseValues=dvh['Dose'])
+    return dvh
+
+
+if __name__ == '__main__':
+
+    # File paths
+    funcs_path = repo_path + 'results\\patient1\\funcs_toy.csv'
+    goals_path = repo_path + 'results\\patient1\\goals.csv'
+    save_path = repo_path + 'results\\patient1\\'
+
+    # Get RayStation objects
+    plan = connect.get_current('Plan')
+    beam_set = connect.get_current('BeamSet')
+
+    # Define functions and goals
+    funcs, goals, roi_names, pars, results, _ = sample.init_prob(funcs_path,
+                                                                 goals_path)
+
+    # Rib Parameters
+    rib_pars = {'Rib': [1600.0, 1609.7507850014442, 1580.0282932223702,
+                        1561.8852225963185]}
+
+    # Calculate plans
+    for ii in range(len(rib_pars['Rib'])):
+        pars = sample.grid_pars(ii, funcs, pars, rib_pars, 4)
+        pars.to_pickle(save_path + 'pars.npy')
+        sample.set_pars(plan, pars)
+        flag = sample.calc_plan(plan, beam_set, 'PTV', 4800, 95)
+        results = sample.get_results(plan, ii, flag, goals, results)
+        results.to_pickle(save_path + 'results.npy')
+        dvh = get_dvh(plan, roi_names)
+        np.save(save_path + f'dvh_{ii}.npy', dvh)
