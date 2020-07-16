0cf9a242b2c4c9dad11bab55dbaac8f545dfa40a
Kelsey Maass <kmaass@uw.edu>
2020-07-15 18:22:31 -0700

    random samples for patient 1

--- src/sample_patient.py
+++ src/sample_patient.py
@@ -5,7 +5,7 @@ import sample
 
 repo_path = '\\\\client\\C$\\Users\\Kelsey\\Dropbox (uwamath)\\autoray\\'
 sys.path.append(repo_path + 'src\\')
-funcs_path = repo_path + 'results\\patient2\\funcs_half.csv'
-goals_path = repo_path + 'results\\patient2\\goals.csv'
-save_path = repo_path + 'results\\patient2\\'
-sample.sample_plans(funcs_path, 'PTV 4/7/20', 6270, 99, goals_path, save_path)
+funcs_path = repo_path + 'results\\patient1\\funcs.csv'
+goals_path = repo_path + 'results\\patient1\\goals.csv'
+save_path = repo_path + 'results\\patient1\\'
+sample.sample_plans(funcs_path, 'PTV', 4800, 95, goals_path, save_path)
