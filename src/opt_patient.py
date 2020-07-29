674b7dcf73314b5963782074fe8f4962557c81a3
Kelsey Maass <kmaass@uw.edu>
2020-07-29 15:33:20 -0700

    added new results and plots for patient 1

--- src/opt_patient.py
+++ src/opt_patient.py
@@ -7,7 +7,7 @@ repo_path = '\\\\client\\C$\\Users\\Kelsey\\Dropbox (uwamath)\\autoray\\'
 sys.path.append(repo_path + 'src\\')
 funcs_path = repo_path + 'results\\patient1\\full\\funcs.csv'
 goals_path = repo_path + 'results\\patient1\\goals.csv'
-save_path = repo_path + 'results\\patient1\\full\\bayes2\\'
-opt.gp_minimize(funcs_path, 'PTV', 4800, 95, goals_path, save_path, n_calls=50)
-#opt.forest_minimize(funcs_path, 'PTV', 4800, 95, goals_path, save_path)
-#opt.dummy_minimize(funcs_path, 'PTV', 4800, 95, goals_path, save_path)
+save_path = repo_path + 'results\\patient1\\full\\rand\\'
+#opt.gp_minimize(funcs_path, 'PTV', 4800, 95, goals_path, save_path, n_calls=50)
+#opt.forest_minimize(funcs_path, 'PTV', 4800, 95, goals_path, save_path, n_calls=50)
+opt.dummy_minimize(funcs_path, 'PTV', 4800, 95, goals_path, save_path, n_calls=50)
