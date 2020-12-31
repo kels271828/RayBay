017834716c75f55fa843e212e1b3a6713390e1ac
Kelsey Maass <kmaass@uw.edu>
2020-12-30 18:20:26 -0800

    separating optimization module and results object

---
+++ src/optimize.py
@@ -0,0 +1,347 @@
+"""RayStation treatment planning with Bayesian optimization.
+
+TODO:
+* Add 1D grid_search function
+
+Note: My virtual environments in Citrix have old version of skopt, so
+kwarg n_initial_points reverted back to n_random_starts
+
+"""
+import re
+
+import numpy as np
+import pandas as pd
+import skopt
+
+import connect
+import raybay
+
+
+def plan_opt(funcs, norm, goals=None, solver='gp_minimize', n_calls=25,
+             random_state=None, n_initial_points=10, verbose=True):
+    """Hyperparameter optimization for RayStation treatment planning.
+
+    Hyperparameter optimization for RayStation treatment planning using
+    the following solvers from scikit-optimize:
+
+        - `gp_minimize`: Bayesian optimization using Gaussian processes.
+        - `forest_minimize`: Sequential optimization using decision
+           trees.
+        - `dummy_minimize`: Random search by uniform sampling within the
+           given bounds.
+
+    For more details about scikit-optimize, refer to
+    https://scikit-optimize.github.io/stable/index.html
+
+    Parameters
+    ----------
+    funcs : str
+        Path to CSV with constituent function specifications.
+    norm : (str, float, float)
+        Region of interest, dose, and volume used for normalization.
+    goals : pandas.DataFrame or str, optional
+        Path to CSV with clinical goal specifications.
+        If None, goals are assigned based on constituent functions.
+    solver : {'gp_minimize', 'forest_minimize', 'dummy_minimize'}
+        Name of scikit-optimize solver to use.
+    n_calls : int, optional
+        Number of calls to objective.
+    random_state : int, optional
+        Set random state for reproducible results.
+    n_initial_points : int, optional
+        Number of random function evaluations before function
+        approximation.
+    verbose : bool, optional
+        Control the verbosity of the solver.
+
+    Returns
+    -------
+    raybay.RaybayResult
+
+    """
+    # Get RayStation objects
+    patient = connect.get_current('Patient')
+    case = connect.get_current('Case')
+    plan = connect.get_current('Plan')
+    beam_set = connect.get_current('BeamSet')
+
+    # Initialize result object
+    result = raybay.RaybayResult(patient.Name, case.CaseName, plan.Name, funcs,
+                                 norm, solver, goals)
+
+    # Optimize
+    obj = lambda pars: objective(plan, beam_set, result.funcs, result.goals,
+                                 norm, result.goal_result, pars)
+    if solver == 'forest_minimize':
+        results = skopt.forest_minimize(obj, dimensions=get_dims(result.funcs),
+                                        n_calls=n_calls,
+                                        n_random_starts=n_initial_points,
+                                        random_state=random_state,
+                                        verbose=verbose)
+    elif solver == 'dummy_minimize':
+        results = skopt.dummy_minimize(obj, dimensions=get_dims(result.funcs),
+                                       n_calls=n_calls,
+                                       n_random_starts=n_initial_points,
+                                       random_state=random_state,
+                                       verbose=verbose)
+    else:
+        results = skopt.gp_minimize(obj, dimensions=get_dims(result.funcs),
+                                    n_calls=n_calls,
+                                    n_random_starts=n_initial_points,
+                                    random_state=random_state, verbose=verbose)
+    result.opt_result = results
+
+    # Get optimal dose-volume histogram
+    set_pars(plan, funcs, result.opt_result.x)
+    calc_plan(plan, beam_set, norm)
+    result.dvh_result = get_dvh(result.roi_list)
+
+    return result
+
+
+def objective(plan, beam_set, funcs, goals, norm, goal_result, pars):
+    """Objective function for hyperparameter optimization.
+
+    Parameters
+    ----------
+    plan : connect.connect_cpython.PyScriptObject
+        Current treatment plan.
+    beam_set : connect.connect_cpython.PyScriptObject
+        Current beam set.
+    funcs : pandas.DataFrame
+        Constituent function specifications.
+    goals : pandas.DataFrame
+        Clinical goal specifications.
+    norm : (str, float, float)
+        Region of interest, dose, and volume used for normalization.
+    goal_result : dict
+        Clinical goal results.
+    pars : list
+        Constituent function parameters.
+
+    Returns
+    -------
+    float
+        Treatment plan score.
+
+    """
+    set_pars(plan, funcs, pars)
+    flag = calc_plan(plan, beam_set, norm)
+    print(f'Flag: {flag}')
+    return get_score(plan, goals, flag, goal_result)
+
+
+def set_pars(plan, funcs, pars):
+    """Set objective function parameters.
+
+    Parameters
+    ----------
+    plan : connect.connect_cpython.PyScriptObject
+        Current treatment plan.
+    funcs : pandas.DataFrame
+        Constituent function specifications.
+    pars : list
+        Constituent function parameters.
+
+    Returns
+    -------
+    None.
+
+    """
+    count = 0
+    const_funcs = plan.PlanOptimizations[0].Objective.ConstituentFunctions
+    for index, row in funcs.iterrows():
+        func = const_funcs[index].DoseFunctionParameters
+        if isinstance(row['DoseLevel'], list):
+            func.DoseLevel = pars[count]
+            count += 1
+        else:
+            func.DoseLevel = row['DoseLevel']
+        if 'Eud' in func.FunctionType:
+            func.EudParameterA = row['EudParameterA']
+        elif isinstance(row['PercentVolume'], list):
+            func.PercentVolume = pars[count]
+            count += 1
+        else:
+            func.PercentVolume = row['PercentVolume']
+        if isinstance(row['Weight'], list):
+            func.Weight = pars[count]
+            count += 1
+        else:
+            func.Weight = row['Weight']
+
+
+def calc_plan(plan, beam_set, norm):
+    """Calculate and normalize treatment plan.
+
+    Parameters
+    ----------
+    plan : connect.connect_cpython.PyScriptObject
+        Current treatment plan.
+    beam_set : connect.connect_cpython.PyScriptObject
+        Current beam set.
+    norm : (str, float, float)
+        Region of interest, dose, and volume used for normalization.
+
+    Returns
+    -------
+    int
+        RayStation exit status:
+        - 0: success
+        - 1: normalization failed
+        - 2: optimization failed
+
+    """
+    # Calculate plan
+    plan.PlanOptimizations[0].ResetOptimization()
+    try:
+        plan.PlanOptimizations[0].RunOptimization()
+    except:
+        return 2
+
+    # Normalize plan
+    try:
+        beam_set.NormalizeToPrescription(RoiName=norm[0], DoseValue=norm[1],
+                                         DoseVolume=norm[2],
+                                         PrescriptionType='DoseAtVolume')
+        return 0
+    except:
+        return 1
+
+
+def get_score(plan, goals, flag, goal_result):
+    """Calculate treatment plan score.
+
+    The treatment plan score is a linear combination of the relative
+    difference between goal values and goal results. Returns 1e6 if
+    RayStation optimization or normalization failed.
+
+    Parameters
+    ----------
+    plan : connect.connect_cpython.PyScriptObject
+        Current treatment plan.
+    goals : pandas.DataFrame
+        Clinical goal specifications.
+    flag : int
+        RayStation exit status.
+    goal_result : dict
+        Clinical goal results.
+
+    Returns
+    -------
+    float
+        Treatment plan score.
+
+    """
+    if flag > 0:
+        return 1e6
+    score = 0
+    results = get_results(plan, goals)
+    for index, row in goals.iterrows():
+        level = row['AcceptanceLevel']
+        value = results[index]
+        goal_result[index].append(value)
+        sign = 1 if 'Most' in row['GoalCriteria'] else -1
+        score += sign*(value - level)/level
+    return score
+
+
+def get_results(plan, goals):
+    """Get clinical goal results.
+
+    Parameters
+    ----------
+    plan : connect.connect_cpython.PyScriptObject
+        Current treatment plan.
+    goals : pandas.DataFrame
+        Clinical goal specifications.
+
+    Returns
+    -------
+    dict
+        Clinical goal results.
+
+    """
+    dose = plan.TreatmentCourse.TotalDose
+    results = {}
+    for index, row in goals.iterrows():
+        results[index] = get_value(dose, row)
+    return results
+
+
+def get_value(dose, goal):
+    """Get clinical goal value.
+
+    Currently able to evaluate MinDose, AverageDose, MaxDose, MinDvh,
+    and MaxDvh clinical goal values. All other clinical goal values are
+    returned as NaN.
+
+    Parameters
+    ----------
+    dose : connect.connect_cpython.PyScriptObject
+        Current treatment plan dose.
+    goal : pandas.core.series.series
+        Row of clinical goal specification DataFrame.
+
+    Returns
+    -------
+    float
+        Clinical goal value.
+
+    """
+    if 'Dose' in goal['Type']:
+        dose_type = re.findall('[A-Z][^A-Z]*', goal['Type'])[0]
+        return dose.GetDoseStatistic(RoiName=goal['Roi'], DoseType=dose_type)
+    elif 'Dvh' in goal['Type']:
+        volume = 0.01*goal['ParameterValue']
+        return dose.GetDoseAtRelativeVolumes(RoiName=goal['Roi'],
+                                             RelativeVolumes=[volume])[0]
+    else:
+        return np.nan
+
+
+def get_dims(funcs):
+    """Get constituent function parameter dimensions.
+
+    Parameters
+    ----------
+    funcs : pandas.DataFrame
+        Constituent function specifications.
+
+    Returns
+    -------
+    list
+        Constituent function parameter dimensions.
+
+    """
+    dimensions = []
+    for _, row in funcs.iterrows():
+        for par in ['DoseLevel', 'PercentVolume', 'Weight']:
+            if isinstance(row[par], list):
+                dimensions.append(row[par])
+    return dimensions
+
+
+def get_dvh(roi_list):
+    """Get dose-volume histogram curves from current plan.
+
+    Parameters
+    ----------
+    roi_list : list of str
+        Regions of interest to include in results.
+
+    Returns
+    -------
+    dict
+        Dose and volumes for given regions of interest.
+
+    """
+    dose = connect.get_current('Plan').TreatmentCourse.TotalDose
+    max_dose = max([dose.GetDoseStatistic(RoiName=roi, DoseType='Max')
+                    for roi in roi_list])
+    dvh_dict = {'Dose': np.linspace(0, max_dose, 100)}
+    for roi in roi_list:
+        vals = dose.GetRelativeVolumeAtDoseValues(RoiName=roi,
+                                                  DoseValues=dvh_dict['Dose'])
+        dvh_dict[roi] = vals
+    return dvh_dict
--- src/raybay.py
+++ src/raybay.py
@@ -1,145 +1,109 @@
 """RayStation treatment planning with Bayesian optimization.
 
-Constituent functions
----------------------
-The constituent function specifications are stored in a DataFrame with
-columns corresponding to each function parameter: Roi, FunctionType,
-DoseLevel, PercentVolume, EudParameterA, and Weight. The row index
-within the DataFrame should correspond to the function index in the
-RayStation objective function. Fixed parameters should be a single
-value, tunable parameters should be a list containing the minimum and
-maximum values, and irrelevant parameters can be left blank. The
-DataFrame can be built manually or from a CSV file using `get_funcs()`.
+Constituent function parameters should be specified by columns Roi,
+FunctionType, DoseLevel, PercentVolume, EudParameterA, and Weight. The
+row index within the table should correspond to the constituent
+function in the RayStation objective. Fixed parameters should be a
+single value, tunable parameters should be a list containing the
+minimum and maximum values, and irrelevant parameters can be left blank.
 
-Clinical goals
---------------
-The clinical goal specifications are stored in a DataFrame with columns
-corresponding to each goal parameter: Roi, Type, GoalCriteria,
-AcceptanceLevel, and ParameterValue. The DataFrame can be built
-manually, based on the minimum or maximum parameter values in the
-constituent functions DataFrame with `get_goals()`, or from a CSV file.
-The function `get_results()` is currently able to evaluate MinDose,
-AverageDose, MaxDose, MinDose, MinDvh, and MaxDvh clinical goals. All
-other clinical goals are not evaluated, and results are set to NaN.
-
-TODO:
-* Add 1D grid_search function
-
-Note: My virtual environments in Citrix have old version of skopt, so
-kwarg n_initial_points reverted back to n_random_starts
+Clinical goals are specified by columns Roi, Type, GoalCriteria,
+AcceptanceLevel, and ParameterValue. Valid types include MinDose,
+AverageDose, MaxDose, MinDose, MinDvh, and MaxDvh.
 
 """
 import re
 
 import numpy as np
 import pandas as pd
-import skopt
-
-import connect
 
 
-def plan_opt(funcs, norm, goals=None, solver='gp_minimize', n_calls=25,
-             random_state=None, n_initial_points=10, verbose=True):
-    """Hyperparameter optimization for RayStation treatment planning.
+class RaybayResult:
+    """Optimized RayStation treatment plan results.
 
-    Hyperparameter optimization for RayStation treatment planning using
-    the following solvers from scikit-optimize:
-
-        - `gp_minimize`: Bayesian optimization using Gaussian processes.
-        - `forest_minimize`: Sequential optimization using decision
-           trees.
-        - `dummy_minimize`: Random search by uniform sampling within the
-           given bounds.
-
-    For more details about scikit-optimize, refer to
-    https://scikit-optimize.github.io/stable/index.html
-
-    Parameters
+    Attributes
     ----------
-    funcs : pandas.DataFrame or str
-        Constituent function specifications or path to CSV file.
+    patient : str
+        Patient name.
+    case : str
+        Case name.
+    plan : str
+        Plan name.
+    funcs : pandas.DataFrame
+        Constituent function specifications.
+    goals : pandas.DataFrame
+        Clinical goal specificaions.
+    roi_list : list of str
+        Regions of interest included in clinical goals.
     norm : (str, float, float)
         Region of interest, dose, and volume used for normalization.
-    goals : pandas.DataFrame or str, optional
-        Clinical goal specifications or path to CSV file.
-        If None, goals are assigned based on constituent functions.
     solver : {'gp_minimize', 'forest_minimize', 'dummy_minimize'}
-        Name of scikit-optimize solver to use.
-    n_calls : int, optional
-        Number of calls to objective.
-    random_state : int, optional
-        Set random state for reproducible results.
-    n_initial_points : int, optional
-        Number of random function evaluations before function
-        approximation.
-    verbose : bool, optional
-        Control the verbosity of the solver.
-
-    Returns
-    -------
-    scipy.optimize.OptimizeResult
-        The optimization results returned as an OptimizeResult object.
-        Important attributes are:
-
-        - `x` [list]: location of the minimum.
-        - `fun` [float]: function value at the minimum.
-        - `models`: surrogate models used for each iteration.
-        - `x_iters` [list of lists]: location of function evaluate for
-           each iteration.
-        - `func_vals` [array]: function value for each iteration.
-        - `space` [Space]: the optimization space.
-        - `specs` [dict]: the call specifications.
-        - `rng` [RandomState instance]: State of the random state at the
-          end of minimization.
-
-        For more details about the OptimizeResult object, refer to
-        http://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.OptimizeResult.html
-    dict
+        Name of scikit-optimize solver used.
+    opt_result : scipy.optimize.OptimizeResult
+        Optimization results.
+    goal_result : dict
         Clinical goal results.
-    dict
-        Final dose-volume histograms.
+    dvh_result : dict
+        Dose-volume histograms of solution.
+
+    Note: The optimization results returned as an OptimizeResult object.
+    Important attributes are:
+
+    - `x` [list]: location of the minimum.
+    - `fun` [float]: function value at the minimum.
+    - `models`: surrogate models used for each iteration.
+    - `x_iters` [list of lists]: location of function evaluate for
+       each iteration.
+    - `func_vals` [array]: function value for each iteration.
+    - `space` [Space]: the optimization space.
+    - `specs` [dict]: the call specifications.
+    - `rng` [RandomState instance]: State of the random state at the
+      end of minimization.
+
+    For more details about the OptimizeResult object, refer to
+    http://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.OptimizeResult.html
 
     """
-    # Get RayStation objects
-    plan = connect.get_current('Plan')
-    beam_set = connect.get_current('BeamSet')
 
-    # Define functions and goals
-    if isinstance(funcs, str):
-        funcs = get_funcs(funcs)
-    if goals is None:
-        goals = get_goals(funcs)
-    elif isinstance(goals, str):
-        goals = pd.read_csv(goals)
-    goal_dict = {ii: [] for ii in range(len(goals))}
+    def __init__(self, patient, case, plan, funcs, norm, solver, goals=None):
+        """Initialise instance of RaybayResult.
 
-    # Optimize
-    obj = lambda pars: objective(plan, beam_set, funcs, goals, norm, goal_dict,
-                                 pars)
-    if solver == 'forest_minimize':
-        results = skopt.forest_minimize(obj, dimensions=get_dimensions(funcs),
-                                        n_calls=n_calls,
-                                        n_random_starts=n_initial_points,
-                                        random_state=random_state,
-                                        verbose=verbose)
-    elif solver == 'dummy_minimize':
-        results = skopt.dummy_minimize(obj, dimensions=get_dimensions(funcs),
-                                       n_calls=n_calls,
-                                       n_random_starts=n_initial_points,
-                                       random_state=random_state,
-                                       verbose=verbose)
-    else:
-        results = skopt.gp_minimize(obj, dimensions=get_dimensions(funcs),
-                                    n_calls=n_calls,
-                                    n_random_starts=n_initial_points,
-                                    random_state=random_state, verbose=verbose)
+        Parameters
+        ----------
+        patient : str
+            Patient name.
+        case : str
+            Case name.
+        plan : str
+            Plan name.
+        funcs : str
+            Path to CSV with constituent function specifications.
+        norm : (str, float, float)
+            Region of interest, dose, and volume used for normalization.
+        solver : {'gp_minimize', 'forest_minimize', 'dummy_minimize'}
+            Name of scikit-optimize solver used.
+        goals : str, optional
+            Path to CSV with clinical goal specifications.
+            If None, goals are assigned based on constituent functions.
 
-    # Get optimal dose-volume histogram
-    set_pars(plan, funcs, results.x)
-    calc_plan(plan, beam_set, norm)
-    dvh_dict = get_dvh(set(goals['Roi']))
+        Returns
+        -------
+        RaybayResult
+            RaybayResult object with plan information.
 
-    return results, goal_dict, dvh_dict
+        """
+        self.patient = patient
+        self.case = case
+        self.plan = plan
+        self.funcs = get_funcs(funcs)
+        self.norm = norm
+        self.solver = solver
+        if isinstance(goals, str):
+            self.goals = pd.read_csv(goals)
+        else:
+            self.goals = get_goals(self.funcs)
+        self.goal_result = {ii : [] for ii in range(len(self.goals))}
+        self.roi_list = set(self.goals['Roi'])
 
 
 def get_funcs(fpath):
@@ -156,7 +120,7 @@ def get_funcs(fpath):
     Parameters
     ----------
     fpath : str
-        Path to CSV file.
+        Path to CSV with constituent function specifications.
 
     Returns
     -------
@@ -180,7 +144,8 @@ def get_goals(funcs):
     """Create clinical goals based on constituent functions.
 
     Clinical goals are specified by columns Roi, Type, GoalCriteria,
-    AcceptanceLevel, and ParameterValue.
+    AcceptanceLevel, and ParameterValue. Valid types include MinDose,
+    AverageDose, MaxDose, MinDose, MinDvh, and MaxDvh.
 
     Parameters
     ----------
@@ -225,251 +190,3 @@ def get_bound(par, func_type):
 
     """
     return np.max(par) if 'Max' in func_type else np.min(par)
-
-
-def objective(plan, beam_set, funcs, goals, norm, goal_dict, pars):
-    """Objective function for hyperparameter optimization.
-
-    Parameters
-    ----------
-    plan : connect.connect_cpython.PyScriptObject
-        Current treatment plan.
-    beam_set : connect.connect_cpython.PyScriptObject
-        Current beam set.
-    funcs : pandas.DataFrame
-        Constituent function specifications.
-    goals : pandas.DataFrame
-        Clinical goal specifications.
-    norm : (str, float, float)
-        Region of interest, dose, and volume used for normalization.
-    goal_dict : dict
-        Clinical goal results.
-    pars : list
-        Constituent function parameters.
-
-    Returns
-    -------
-    float
-        Treatment plan score.
-
-    """
-    set_pars(plan, funcs, pars)
-    flag = calc_plan(plan, beam_set, norm)
-    print(f'Flag: {flag}')
-    return get_score(plan, goals, flag, goal_dict)
-
-
-def set_pars(plan, funcs, pars):
-    """Set objective function parameters.
-
-    Parameters
-    ----------
-    plan : connect.connect_cpython.PyScriptObject
-        Current treatment plan.
-    funcs : pandas.DataFrame
-        Constituent function specifications.
-    pars : list
-        Constituent function parameters.
-
-    Returns
-    -------
-    None.
-
-    """
-    count = 0
-    const_funcs = plan.PlanOptimizations[0].Objective.ConstituentFunctions
-    for index, row in funcs.iterrows():
-        func = const_funcs[index].DoseFunctionParameters
-        if isinstance(row['DoseLevel'], list):
-            func.DoseLevel = pars[count]
-            count += 1
-        else:
-            func.DoseLevel = row['DoseLevel']
-        if 'Eud' in func.FunctionType:
-            func.EudParameterA = row['EudParameterA']
-        elif isinstance(row['PercentVolume'], list):
-            func.PercentVolume = pars[count]
-            count += 1
-        else:
-            func.PercentVolume = row['PercentVolume']
-        if isinstance(row['Weight'], list):
-            func.Weight = pars[count]
-            count += 1
-        else:
-            func.Weight = row['Weight']
-
-
-def calc_plan(plan, beam_set, norm):
-    """Calculate and normalize treatment plan.
-
-    Parameters
-    ----------
-    plan : connect.connect_cpython.PyScriptObject
-        Current treatment plan.
-    beam_set : connect.connect_cpython.PyScriptObject
-        Current beam set.
-    norm : (str, float, float)
-        Region of interest, dose, and volume used for normalization.
-
-    Returns
-    -------
-    int
-        RayStation exit status:
-        - 0: success
-        - 1: normalization failed
-        - 2: optimization failed
-
-    """
-    # Calculate plan
-    plan.PlanOptimizations[0].ResetOptimization()
-    try:
-        plan.PlanOptimizations[0].RunOptimization()
-    except:
-        return 2
-
-    # Normalize plan
-    try:
-        beam_set.NormalizeToPrescription(RoiName=norm[0], DoseValue=norm[1],
-                                         DoseVolume=norm[2],
-                                         PrescriptionType='DoseAtVolume')
-        return 0
-    except:
-        return 1
-
-
-def get_score(plan, goals, flag, goal_dict):
-    """Calculate treatment plan score.
-
-    The treatment plan score is a linear combination of the relative
-    difference between goal values and goal results. Returns 1e6 if
-    RayStation optimization or normalization failed.
-
-    Parameters
-    ----------
-    plan : connect.connect_cpython.PyScriptObject
-        Current treatment plan.
-    goals : pandas.DataFrame
-        Clinical goal specifications.
-    flag : int
-        RayStation exit status.
-    goal_dict : dict
-        Clinical goal results.
-
-    Returns
-    -------
-    float
-        Treatment plan score.
-
-    """
-    if flag > 0:
-        return 1e6
-    score = 0
-    results = get_results(plan, goals)
-    for index, row in goals.iterrows():
-        level = row['AcceptanceLevel']
-        value = results[index]
-        goal_dict[index].append(value)
-        sign = 1 if 'Most' in row['GoalCriteria'] else -1
-        score += sign*(value - level)/level
-    return score
-
-
-def get_results(plan, goals):
-    """Get clinical goal results.
-
-    Parameters
-    ----------
-    plan : connect.connect_cpython.PyScriptObject
-        Current treatment plan.
-    goals : pandas.DataFrame
-        Clinical goal specifications.
-
-    Returns
-    -------
-    dict
-        Clinical goal results.
-
-    """
-    dose = plan.TreatmentCourse.TotalDose
-    results = {}
-    for index, row in goals.iterrows():
-        results[index] = get_value(dose, row)
-    return results
-
-
-def get_value(dose, goal):
-    """Get clinical goal value.
-
-    Currently able to evaluate MinDose, AverageDose, MaxDose, MinDvh,
-    and MaxDvh clinical goal values. All other clinical goal values are
-    returned as NaN.
-
-    Parameters
-    ----------
-    dose : connect.connect_cpython.PyScriptObject
-        Current treatment plan dose.
-    goal : pandas.core.series.series
-        Row of clinical goal specification DataFrame.
-
-    Returns
-    -------
-    float
-        Clinical goal value.
-
-    """
-    if 'Dose' in goal['Type']:
-        dose_type = re.findall('[A-Z][^A-Z]*', goal['Type'])[0]
-        return dose.GetDoseStatistic(RoiName=goal['Roi'], DoseType=dose_type)
-    elif 'Dvh' in goal['Type']:
-        volume = 0.01*goal['ParameterValue']
-        return dose.GetDoseAtRelativeVolumes(RoiName=goal['Roi'],
-                                             RelativeVolumes=[volume])[0]
-    else:
-        return np.nan
-
-
-def get_dimensions(funcs):
-    """Get constituent function parameter dimensions.
-
-    Parameters
-    ----------
-    funcs : pandas.DataFrame
-        Constituent function specifications.
-
-    Returns
-    -------
-    list
-        Constituent function parameter dimensions.
-
-    """
-    dimensions = []
-    for _, row in funcs.iterrows():
-        for par in ['DoseLevel', 'PercentVolume', 'Weight']:
-            if isinstance(row[par], list):
-                dimensions.append(row[par])
-    return dimensions
-
-
-def get_dvh(roi_list):
-    """Get dose-volume histogram curves from current plan.
-
-    Parameters
-    ----------
-    roi_list : list of str
-        Regions of interest to include in results.
-
-    Returns
-    -------
-    dict
-        Dose and volumes for given regions of interest.
-
-    """
-    dose = connect.get_current('Plan').TreatmentCourse.TotalDose
-    max_dose = max([dose.GetDoseStatistic(RoiName=roi, DoseType='Max')
-                    for roi in roi_list])
-    dvh_dict = {'Dose': np.linspace(0, max_dose, 100)}
-    for roi in roi_list:
-        vals = dose.GetRelativeVolumeAtDoseValues(RoiName=roi,
-                                                  DoseValues=dvh_dict['Dose'])
-        dvh_dict[roi] = vals
-    return dvh_dict
