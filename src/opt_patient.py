76d697302c554bfb62de548015bb3b1fe9857f6f
Kelsey Maass <kmaass@uw.edu>
2020-12-30 20:16:47 -0800

    comments about saving

--- src/opt_patient.py
+++ src/opt_patient.py
@@ -1,5 +1,5 @@
 """Optimize treatment plans for patient."""
-import pickle
+import dill
 import sys
 
 import optimize
@@ -10,5 +10,17 @@ funcs_path = repo_path + 'results\\SBRT_lung_minsun\\funcs_small.csv'
 goals_path = repo_path + 'results\\SBRT_lung_minsun\\goals.csv'
 save_path = repo_path + 'results\\SBRT_lung_minsun\\'
 result = optimize.plan_opt(funcs_path, ('PTV', 4800, 95), goals_path,
-                           n_calls=5, n_initial_points=2)
-pickle.dump(result, open(save_path + 'results', 'wb'))
+                           n_calls=3, n_initial_points=2)
+dill.dump(result, open(save_path + 'result', 'wb'))
+
+# can't save local objects... nested functions...
+# result.opt_result.specs['args']['funcs'] = 'local'
+# removes what was 
+# <function optimize.plan_opt.<locals>.obj(pars)>
+# which was preventing it from being saved...
+# 
+# would pickle also still work?
+# yes, don't need dill anymore...
+
+# can't figure out how else to pass function to gp_minimize
+# other than inner function or lambda...
