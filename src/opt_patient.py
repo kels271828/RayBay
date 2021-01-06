698c007d9952630ebd686e661e108356a80c5c27
Kelsey Maass <kmaass@uw.edu>
2021-01-05 20:15:56 -0800

    added output logging

--- src/opt_patient.py
+++ src/opt_patient.py
@@ -1,14 +1,41 @@
-"""Optimize treatment plans for patient."""
+"""Optimize treatment plans for patient.
+
+Current workflow:
+    * In spyder preferences, select
+        Default working directory: The directory of the file being executed
+    * Import `connect` module while still in working directory
+        C:\Program Files\RaySearch Laboratories\RayStation 8B SP1\ScriptClient
+      before running the script.
+
+I am getting new error
+    "Qt Qtwebengineprocess has stopped working"
+when I open spyder and when I connect to the kernel.
+Not sure if it's important?
+
+"""
 import pickle
 import sys
 
 import optimize
 
-repo_path = '\\\\client\\C$\\Users\\Kelsey\\Dropbox (uwamath)\\autoray\\'
-sys.path.append(repo_path + 'src\\')
-funcs_path = repo_path + 'results\\SBRT_lung_minsun\\funcs_small.csv'
-goals_path = repo_path + 'results\\SBRT_lung_minsun\\goals.csv'
-save_path = repo_path + 'results\\SBRT_lung_minsun\\'
-result = optimize.get_plan(funcs_path, ('PTV', 4800, 95), goals_path,
-                           n_calls=3, n_initial_points=2)
-pickle.dump(result, open(save_path + 'result', 'wb'))
+# Setup
+patient = '..\\results\\SBRT_lung_minsun\\'
+case = 'bayes_pars_2\\'
+
+# Optimize treatment plan
+with open(patient + case + 'log.txt', 'w') as fp:
+    sys.stdout = fp
+    result = optimize.get_plan(
+        funcs=patient + case + 'funcs.csv',
+        norm=('PTV', 4800, 95),
+        goals=patient + 'goals.csv',
+        solver='gp_minimize',
+        n_calls=50,
+        random_state=7,
+        n_initial_points=10,
+        verbose=True
+    )
+
+# Save results
+with open(patient + case + 'result', 'wb') as fp:
+    pickle.dump(result, fp)
