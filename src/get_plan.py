289aba26197f7bc25c5897e7503b0829e8dcf67c
Kelsey Maass <kmaass@uw.edu>
2021-04-08 13:00:26 -0700

    adding normalization

--- src/get_plan.py
+++ src/get_plan.py
@@ -4,17 +4,18 @@ import sys
 
 import connect
 
-repo_path = '\\\\client\\C$\\Users\\Kelsey\\Dropbox (uwamath)\\raybay\\'
+repo_path = '\\\\client\\E$\\My Drive\\RayBay\\'
 sys.path.append(repo_path + 'src\\')
 import optimize
 import raybay
 
 # Patient
-patient_path = repo_path + 'results\\SBRT_lung_minsun\\'
+#patient_path = repo_path + 'results\\SBRT_lung_minsun\\'
 #patient_path = repo_path + 'results\\ZZ_MK_LLungSBRT3778\\'
 #patient_path = repo_path + 'results\\ZZ_MK_RLungSBRT4076\\'
 #patient_path = repo_path + 'results\\ZZ_MK_RULungSBRT3796\\'
 #patient_path = repo_path + 'results\\ZZ_MK_LLLungSBRT3977\\'
+patient_path = repo_path + 'results\\ZZ_MK_RLSBRT1931\\'
 
 # Case
 case_path = 'approved\\'
@@ -33,7 +34,7 @@ result = raybay.RaybayResult(
     plan.Name,
     patient_path + case_path + 'funcs.csv',
     ('PTV', 4800, 95),
-    goals=patient_path + 'goals.csv')
+    goals=patient_path + case_path + 'goals.csv')
 
 # Add results
 if 'default' in case_path:
@@ -43,9 +44,12 @@ else:
     flag = 0
 result.flag_list.append(flag)
 goal_results = optimize.get_results(plan, result.goal_df)
+scale = optimize.get_scale(result.goal_df, result.norm, goal_results)
 for index, row in result.goal_df.iterrows():
-    result.goal_dict[index].append(goal_results[index])
+    result.goal_dict[index].append(scale*goal_results[index])
 result.dvh_dict = optimize.get_dvh(result.roi_list)
+coeff = result.goal_dict[6][0]/result.dvh_dict['Dose'][-1]
+result.dvh_dict['Dose'] *= coeff  # normalize (check index in prev line)
 
 # Save results
 result_path = patient_path + case_path + 'res_' + case_path[:-1] + '.pkl'
