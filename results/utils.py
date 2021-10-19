4ede36770794615a8e932f8118be6b92778f3e71
Kelsey Maass <kmaass@uw.edu>
2021-10-19 16:48:22 -0700

    add time results with stopping conditions

--- results/utils.py
+++ results/utils.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 patients = [
     'SBRT_lung_minsun',
     'ZZ_MK_LLungSBRT3778',
@@ -27,6 +29,7 @@ goal_names = [
     '11_SpinalCord_MaxDose'
 ]
 
+
 def get_plan_path(plan_type):
     if plan_type == 'clinical':
         return '/approved/res_approved.pkl'
@@ -37,3 +40,74 @@ def get_plan_path(plan_type):
     if plan_type == 'bayes':
         return '/bayes/res_linquad_gp_minimize.pkl'
     raise Exception(f"Invalid plan_type '{plan_type}'.")
+
+
+def get_plan_time(patient, plan_type, stop=False):
+    """Get planning time."""
+    if plan_type in ['clinical', 'default']:
+        raise Exception(f"Time not recorded for {plan_type} plans.")
+    if stop:
+        return get_stop_time(patient, plan_type)
+    plan = np.load(patient + get_plan_path(plan_type), allow_pickle=True)
+    return plan.time/3600.0
+
+
+def get_stop_time(patient, plan_type):
+    """Get planning time with stopping condition."""
+    plan = np.load(patient + get_plan_path(plan_type), allow_pickle=True)
+    util_vec = plan.opt_result.func_vals
+    ii = get_stop_idx(util_vec)
+    return get_log_time(ii, patient, plan_type)
+
+
+def get_stop_idx(util_vec, n=20, m=15, p=1):
+    """Get index of last iteration based on stopping condition."""
+    best_util = np.minimum.accumulate(util_vec)
+    for ii in range(n - 1, len(best_util)):
+        max_util = best_util[ii - m + 1]
+        min_util = best_util[ii]
+        if 100*np.abs((min_util - max_util)/max_util) < p:
+            return ii
+    return len(best_util) - 1
+
+
+def get_log_time(ii, patient, plan_type):
+    """Get planning time used for `ii` iterations."""
+    with open(patient + get_log_path(plan_type)) as f:
+        log = f.readlines()
+    count = 0
+    total_time = 0
+    for row in log:
+        if 'Time' in row:
+            total_time += float(row.split()[-1])
+            count += 1
+        if count > ii:
+            return total_time/3600.0
+    return total_time/3600.0
+
+
+def get_log_path(plan_type):
+    log_path = get_plan_path(plan_type).replace('res', 'log')
+    return log_path.replace('pkl', 'txt')
+
+
+def get_best_idx(patient, plan_type, stop=False, n=20, m=15, p=1):
+    """Get index of best utility with based on stopping condition."""
+    if plan_type in ['clinical', 'default']:
+        return 0
+    plan = np.load(patient + get_plan_path(plan_type), allow_pickle=True)
+    util_vec = plan.opt_result.func_vals
+    if stop:
+        stop_idx = get_stop_idx(util_vec)
+        util_vec = util_vec[:stop_idx + 1]
+    return np.argmin(util_vec)
+
+
+
+
+
+
+
+
+
+
